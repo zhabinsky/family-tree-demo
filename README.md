13b15d5bcc7f696ce2a40c148164678f49148fe8
Vlad Zhabinsky <vladislavzhabinsky@gmail.com>
2020-10-11 17:56:02 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Family Tree Demo
+
+
+[Live version]()
---
+++ docs/precache-manifest.5710bd56c5a689ae90413984787e25c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5748c90a8ad0f87bf8202cac83d1b11a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd8ad24d064f32f6b0d6",
+    "url": "/static/css/main.7bf65dd0.chunk.css"
+  },
+  {
+    "revision": "30b6125b870ff1d25977",
+    "url": "/static/js/2.7f75a2d8.chunk.js"
+  },
+  {
+    "revision": "ccb07d3f36c83d118d798038f894bf02",
+    "url": "/static/js/2.7f75a2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd8ad24d064f32f6b0d6",
+    "url": "/static/js/main.aa6d20f4.chunk.js"
+  },
+  {
+    "revision": "f178e071aa26cf3ce059",
+    "url": "/static/js/runtime-main.508d76fc.js"
+  }
+]);
